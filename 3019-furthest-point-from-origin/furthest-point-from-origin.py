edd5cc2f4f1d98b6e1abe68996a623b332ef6b96
meghanashalivaha <kummarim437@gmail.com>
2026-04-24 20:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (84.21%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.py
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -1,7 +1,3 @@
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
-        left = moves.count('L')
-        right = moves.count('R')
-        blank = moves.count('_')
-        
-        return abs(right - left) + blank
+        return abs(moves.count("R") - moves.count("L")) + moves.count("_")
